b438a1af0c744d2862bac93858e266e7c969b268
walfonso <walfonso@hotmail.com>
2023-12-06 01:24:37 -0300

    update axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  //baseURL: "https://dbmarket-2023-3d2ee059ea6d.herokuapp.com/api",
+  baseURL: "https://dbmarket-2023-3d2ee059ea6d.herokuapp.com/api",
   //baseURL: "http://127.0.0.1:3001/api",
 });
 
