ea7869caebf0c88de75a899c95ab20573ce0c63a
walfonso <walfonso@hotmail.com>
2023-12-06 03:45:48 -0300

    update PrivateRoute

--- src/auth/PrivateRoutes.js
+++ src/auth/PrivateRoutes.js
@@ -2,14 +2,14 @@ import { Route, Redirect } from "react-router-dom";
 
 import { useSelector } from "react-redux";
 
-export const PrivateRoute = ({ component: Component }) => {
+export const PrivateRoute = ({ component: Component, ...rest }) => {
   const user = useSelector((state) => state.authState);
   console.log("estoy aca", user);
   return (
     <Route
-      //{...rest}
+      {...rest}
       render={(props) =>
-        user.email ? <Component /> : <Redirect to="/users/login" />
+        user.email ? <Component {...props} /> : <Redirect to="/users/login" />
       }
     />
   );
