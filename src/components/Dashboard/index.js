590bea5df46f4f87b2c2c2238fe3bb7aebb3baa3
walfonso <walfonso@hotmail.com>
2023-12-07 13:38:54 -0300

    add auth Dashboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+//function getUser() {
+//  let user = localStorage.getItem("user");
+//  if (user) {
+//    user = JSON.parse(user);
+//  } else {
+//    user = null;
+//  }
+//  return user;
+//}
 
-function getUser() {
-  let user = localStorage.getItem("user");
-  if (user) {
-    user = JSON.parse(user);
-  } else {
-    user = null;
-  }
-  return user;
-}
-
+const user = useSelector((state) => state.authState);
 const Dashboard = () => {
-  const [user, setUser] = useState(getUser());
-
+  //const [user, setUser] = useState(getUser());
+  const history = useHistory();
+  if (!user.mail) {
+    history.push(`/users/login`);
+  }
   return (
     <div className="row justify-content-center">
       <div className="col-md-8 p-4">
